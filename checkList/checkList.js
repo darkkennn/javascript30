8cab2fadc373c008286ca9c53738d0292aa2dc7d
darkkennn <sacpan2000@gmail.com>
2024-11-17 20:16:19 +0530

    checklist completed

--- checkList/checkList.js
+++ checkList/checkList.js
@@ -1,8 +1,23 @@
-const items = document.querySelectorAll('item');
+const items = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-function markChecked() {
-    console.log('checking');
-    items.classList.add('checked');
+let lastChecked;
+
+function markChecked(e) {
+    let inBetween = false;
+    console.log(this.checked);
+    if(e.shiftKey && this.checked){
+        items.forEach(item => {
+            console.log(item);
+            if(item === this || item === lastChecked){
+                inBetween = !inBetween;
+            };
+
+            if(inBetween){
+                item.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
 }
 
-items.addEventListener('mousedown', markChecked);
+items.forEach(item => item.addEventListener('click', markChecked));
